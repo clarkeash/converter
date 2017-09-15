7c7d0c305d0666ff74aabd3058aadb4a0628e1cf
Ashley Clarke <me@ashleyclarke.me>
2017-09-15 21:59:32 +0200

    test converter

---
+++ tests/Unit/ConvertTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Clarkeash\Converter\Tests\Unit;
+
+use Clarkeash\Converter\Convert;
+use Clarkeash\Converter\From;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class ConvertTest extends TestCase
+{
+    /**
+     * @test
+     * @expectedException \BadMethodCallException
+     */
+    public function it_errors_if_no_metric_exists()
+    {
+        Convert::nope();
+    }
+
+    /** @test */
+    public function it_returns_an_instance_of_from()
+    {
+        Assert::assertInstanceOf(From::class, Convert::size());
+        Assert::assertInstanceOf(From::class, (new Convert)->size());
+    }
+
+    /** @test */
+    public function it_can_get_and_set_a_value()
+    {
+        $convert = new Convert;
+        Assert::assertEquals(0, $convert->getValue());
+
+        $convert->setValue(20);
+        Assert::assertEquals(20, $convert->getValue());
+    }
+}
