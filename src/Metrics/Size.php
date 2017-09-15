89d65bf3b439c780a94d95c2650ff023c78ed498
Ashley Clarke <me@ashleyclarke.me>
2017-09-15 21:30:33 +0200

    add more sizes

--- src/Metrics/Size.php
+++ src/Metrics/Size.php
@@ -21,54 +21,163 @@ class Size implements Metric
         return pow(2, 3);
     }
 
-    public function kibibytes()
+    public function kilobits()
     {
-        return $this->bytes() * pow(1024, 1);
+        return pow(1000, 1);
     }
 
-    public function mebibytes()
+    public function kibibits()
     {
-        return $this->bytes() * pow(1024, 2);
+        return pow(1024, 1);
     }
-
-    public function gibibytes()
-    {
-        return $this->bytes() * pow(1024, 3);
-    }
-
-    public function tebibytes()
-    {
-        return $this->bytes() * pow(1024, 4);
-    }
-
-    public function pebibytes()
-    {
-        return $this->bytes() * pow(1024, 5);
-    }
-
 
     public function kilobytes()
     {
-        return $this->bytes() * pow(1000, 1);
+        return $this->bytes() * $this->kilobits();
+    }
+
+    public function kibibytes()
+    {
+        return $this->bytes() * $this->kibibits();
+    }
+
+    public function megabits()
+    {
+        return pow(1000, 2);
+    }
+
+    public function mebibits()
+    {
+        return pow(1024, 2);
     }
 
     public function megabytes()
     {
-        return $this->bytes() * pow(1000, 2);
+        return $this->bytes() * $this->megabits();
+    }
+
+    public function mebibytes()
+    {
+        return $this->bytes() * $this->mebibits();
+    }
+
+    public function gigabits()
+    {
+        return pow(1000, 3);
+    }
+
+    public function gibibits()
+    {
+        return pow(1024, 3);
     }
 
     public function gigabytes()
     {
-        return $this->bytes() * pow(1000, 3);
+        return $this->bytes() * $this->gigabits();
+    }
+
+    public function gibibytes()
+    {
+        return $this->bytes() * $this->gibibits();
+    }
+
+    public function terabits()
+    {
+        return pow(1000, 4);
+    }
+
+    public function tebibits()
+    {
+        return pow(1024, 4);
     }
 
     public function terabytes()
     {
-        return $this->bytes() * pow(1000, 4);
+        return $this->bytes() * $this->terabits();
+    }
+
+    public function tebibytes()
+    {
+        return $this->bytes() * $this->tebibits();
+    }
+
+    public function petabits()
+    {
+        return pow(1000, 5);
+    }
+
+    public function pebibits()
+    {
+        return pow(1024, 5);
     }
 
     public function petabytes()
     {
-        return $this->bytes() * pow(1000, 5);
+        return $this->bytes() * $this->petabits();
+    }
+
+    public function pebibytes()
+    {
+        return $this->bytes() * $this->pebibits();
+    }
+
+    public function exabits()
+    {
+        return pow(1000, 6);
+    }
+
+    public function exbibits()
+    {
+        return pow(1024, 6);
+    }
+
+    public function exabytes()
+    {
+        return $this->bytes() * $this->exabits();
+    }
+
+    public function exbibytes()
+    {
+        return $this->bytes() * $this->exbibits();
+    }
+
+    public function zettabits()
+    {
+        return pow(1000, 7);
+    }
+
+    public function zebibits()
+    {
+        return pow(1024, 7);
+    }
+
+    public function zettabytes()
+    {
+        return $this->bytes() * $this->zettabits();
+    }
+
+    public function zebibytes()
+    {
+        return $this->bytes() * $this->zebibits();
+    }
+
+    public function yottabits()
+    {
+        return pow(1000, 8);
+    }
+
+    public function yobibits()
+    {
+        return pow(1024, 8);
+    }
+
+    public function yottabytes()
+    {
+        return $this->bytes() * $this->yottabits();
+    }
+
+    public function yobibytes()
+    {
+        return $this->bytes() * $this->yobibits();
     }
 }
--- tests/Unit/SizeTest.php
+++ tests/Unit/SizeTest.php
@@ -17,19 +17,52 @@ class SizeTest extends TestCase
         Assert::assertEquals(4, $size->nibbles());
         Assert::assertEquals(8, $size->bytes());
 
-        Assert::assertEquals(8000, $size->kilobytes());
-        Assert::assertEquals(8192, $size->kibibytes());
+        // K
+        Assert::assertEquals($kilobits = 1000, $size->kilobits());
+        Assert::assertEquals($kibibits = 1024, $size->kibibits());
+        Assert::assertEquals($kilobytes = 8000, $size->kilobytes());
+        Assert::assertEquals($kibibytes = 8192, $size->kibibytes());
 
-        Assert::assertEquals(8000000, $size->megabytes());
-        Assert::assertEquals(8388608, $size->mebibytes());
+        // M
+        Assert::assertEquals($megabits = $kilobits * 1000, $size->megabits());
+        Assert::assertEquals($mebibits = $kibibits * 1024, $size->mebibits());
+        Assert::assertEquals($megabytes = $kilobytes * 1000, $size->megabytes());
+        Assert::assertEquals($mebibytes = $kibibytes * 1024, $size->mebibytes());
 
-        Assert::assertEquals(8000000000, $size->gigabytes());
-        Assert::assertEquals(8589934592, $size->gibibytes());
+        // G
+        Assert::assertEquals($gigabits = $megabits * 1000, $size->gigabits());
+        Assert::assertEquals($gibibits = $mebibits * 1024, $size->gibibits());
+        Assert::assertEquals($gigabytes = $megabytes * 1000, $size->gigabytes());
+        Assert::assertEquals($gibibytes = $mebibytes * 1024, $size->gibibytes());
 
-        Assert::assertEquals(8000000000000, $size->terabytes());
-        Assert::assertEquals(8796093022208, $size->tebibytes());
+        // T
+        Assert::assertEquals($terabits = $gigabits * 1000, $size->terabits());
+        Assert::assertEquals($tebibits = $gibibits * 1024, $size->tebibits());
+        Assert::assertEquals($terabytes = $gigabytes * 1000, $size->terabytes());
+        Assert::assertEquals($tebibytes = $gibibytes * 1024, $size->tebibytes());
 
-        Assert::assertEquals(8000000000000000, $size->petabytes());
-        Assert::assertEquals(9007199254740992, $size->pebibytes());
+        // P
+        Assert::assertEquals($petabits = $terabits * 1000, $size->petabits());
+        Assert::assertEquals($pebibits = $tebibits * 1024, $size->pebibits());
+        Assert::assertEquals($petabytes = $terabytes * 1000, $size->petabytes());
+        Assert::assertEquals($pebibytes = $tebibytes * 1024, $size->pebibytes());
+
+        // E
+        Assert::assertEquals($exabits = $petabits * 1000, $size->exabits());
+        Assert::assertEquals($exbibits = $pebibits * 1024, $size->exbibits());
+        Assert::assertEquals($exabytes = $petabytes * 1000, $size->exabytes());
+        Assert::assertEquals($exbibytes = $pebibytes * 1024, $size->exbibytes());
+
+        // Z
+        Assert::assertEquals($zettabits = $exabits * 1000, $size->zettabits());
+        Assert::assertEquals($zebibits = $exbibits * 1024, $size->zebibits());
+        Assert::assertEquals($zettabytes = $exabytes * 1000, $size->zettabytes());
+        Assert::assertEquals($zebibytes = $exbibytes * 1024, $size->zebibytes());
+
+        // Y
+        Assert::assertEquals($yottabits = $zettabits * 1000, $size->yottabits());
+        Assert::assertEquals($yobibits = $zebibits * 1024, $size->yobibits());
+        Assert::assertEquals($yottabytes = $zettabytes * 1000, $size->yottabytes());
+        Assert::assertEquals($yobibytes = $zebibytes * 1024, $size->yobibytes());
     }
 }
